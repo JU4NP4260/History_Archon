223c9c48a64772e764afd9238ac349c2650ebd9e
JU4NP4260 <82065985+JU4NP4260@users.noreply.github.com>
2021-10-13 22:11:38 -0500

    INTENTO de AI Targetting

--- Assets/Scripts/2- Enemies/Basic_Enemy.cs
+++ Assets/Scripts/2- Enemies/Basic_Enemy.cs
@@ -1,16 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class Basic_Enemy : MonoBehaviour
 {
+    [Header("Health")]
     public int maxHealth = 15;
     int currentHealth;
+
+    [Header("Pathfinidng")]
+    public Transform target;
+    public float activeDistance = 20f;
+    public float pathUpdateSeconds = 0.5f;
+
+    [Header("Physics")]
+    public float speed = 100f;
+    public float nextWayPointDistance = 3f;
+    public float jumpNodeHeightRequirement = 0.8f;
+    public float jumpModifier = 0.3f;
+    public float jumpCheckOffSet = 0.1f;
+
+    [Header("Custom Behavior")]
+    public bool followEnabled = true;
+    public bool jumpEnabled = true;
+    public bool directionLookEnabled = true;
+
+    private Path path;
+    private int currentWayPoint = 0;
+    bool isGrounded = false;
+    Seeker seeker;
+    Rigidbody2D rb;
 
 
     void Start()
     {
         currentHealth = maxHealth;
+
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        InvokeRepeating("UpdatePath", 0f, pathUpdateSeconds);
     }
 
     public void TakeDamage(int meleeDamage)
@@ -26,6 +56,82 @@ public class Basic_Enemy : MonoBehaviour
         }
     }
 
+
+    private void FixedUpdate()
+    {
+        if(TargetInDistance() && followEnabled)
+        {
+            PathFollow();
+        }
+    }
+
+    private void UpdatePath()
+    {
+        if (followEnabled && TargetInDistance() && seeker.IsDone())
+        {
+            seeker.StartPath(rb.position, target.position, OnPathComplete);
+        }
+    }
+
+    private void PathFollow()
+    {
+        if(path == null)
+        {
+            return;
+        }
+
+        if(currentWayPoint >= path.vectorPath.Count)
+        {
+            return;
+        }
+
+        isGrounded = Physics2D.Raycast(transform.position, -Vector3.up, GetComponent<Collider2D>().bounds.extents.y + jumpCheckOffSet);
+
+        Vector2 direction = ((Vector2)path.vectorPath[currentWayPoint] - rb.position).normalized;
+        Vector2 force = direction * speed * Time.deltaTime;
+
+        if(jumpEnabled && isGrounded)
+        {
+            if(direction.y > jumpNodeHeightRequirement)
+            {
+                rb.AddForce(Vector2.up * speed * jumpModifier);
+            }
+        }
+
+        rb.AddForce(force);
+
+        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWayPoint]);
+        if(distance < nextWayPointDistance)
+        {
+            currentWayPoint++;
+        }
+
+        if (directionLookEnabled)
+        {
+            if(rb.velocity.x > 0.05f)
+            {
+                transform.localScale = new Vector3(-1 * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+            else if(rb.velocity.x < -0.05f)
+            {
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+            }
+        }
+    }
+
+    private bool TargetInDistance()
+    {
+        return Vector2.Distance(transform.position, target.transform.position) < activeDistance;
+    }
+
+    private void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWayPoint = 0;
+        }
+    }
 
     void Update()
     {
