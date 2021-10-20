34610bb8a4ba923be9e59e6c05148e11373c5b0b
Juan Fer <83317226+Fercosmicking@users.noreply.github.com>
2021-10-20 15:43:14 -0500

    A LOT OF CHANGES
    idk idk idk idk
    marikad1tas varías
    Canvas added - Showcase the Win Screen
    There's a script for the animator -> UNUSED.

---
+++ Assets/LevelLoader.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 5f; 
+
+   
+}
